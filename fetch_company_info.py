e07e195166d0898b7bdf555719ba4391a35b76b3
minhquach8 <minhquach8@gmail.com>
2021-12-10 03:19:56 +0700

    Minh updates 10/12/21 -2

--- fetch_company_info.py
+++ fetch_company_info.py
@@ -1,13 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
-import csv
+import csv, os
+
+# Folder CSV to save all company's info csv files
+# stock_exchanges includes three main VN stock exchanges
+stock_exchanges = ["hose", "hnx", "upcom"]
+
+# Check if CSV folder exists or not
+if not os.path.exists(path):
+    os.makedirs(path)
 
 # Function: Read stocks from file text
 def read_stocks_text_file(namefile):
     """
     Read stocks from a text file, remove end-line breaks, convert them into a list
     """
-    file = open(f"{namefile}.txt", "r")
+    file = open(f".\stocktickers\{namefile}.txt", "r")
     content = file.read()
     stocks_list = content.split(", ")
     file.close()
@@ -21,18 +29,29 @@ def fetch_company_info(ticker):
     doc = BeautifulSoup(result.text, "html.parser")
     # Get category
     category = doc.find("div", {"class": "industry"}).a.text
-    # Get Company brief info
+    # Get company brief info
     info = doc.find("div", {"class": "text_content"}).text
     # Get company address
     addr = doc.find("td", {"valign": "top"}).b.next_sibling
     # Get company link
     link = doc.find_all("td", {"class": "right"})[2].a.text
-    return [ticker, category.strip(), info.strip(), addr.strip(), link.strip()]
+    return [
+        ticker,
+        category.strip(),
+        info.strip(),
+        addr.strip(),
+        link.strip(),
+    ]
 
 
 # Function: Create a csv file
 def create_company_info_csv(namefile):
-    csv_file = open(f"{namefile}.csv", "w", encoding="utf-8", newline="")
+    csv_file = open(
+        f".\CSV\{namefile}_companies.csv",
+        "w",
+        encoding="utf-8",
+        newline="",
+    )
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(["Ticker", "Category", "Info", "Address", "Link"])
 
@@ -43,6 +62,9 @@ def create_company_info_csv(namefile):
     csv_file.close()
 
 
-create_company_info_csv("hose")
-# create_company_info_csv("upcom")
-# create_company_info_csv("hnx")
+# Main function
+def main():
+    for stock_exchange in stock_exchanges:
+        create_company_info_csv(stock_exchange)
+
+main()
